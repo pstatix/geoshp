ae12f8271dc5be764d6b696f24f2fe480bda0bad
pstatix <paul.statham.91@gmail.com>
2022-06-07 23:22:56 -0400

    Added Factory for ShapefileInterface subclasses by mode and shape type

--- src/geoshp/shp/factory.py
+++ src/geoshp/shp/factory.py
@@ -31,5 +31,23 @@ class _ModeFactory(_ModeFactoryBase):
 
 
 def register_mode(mode: str, _type: int, kls: typing.Type[shp.ShapefileInterface]) -> None:
+    """Register a class type based on the required mode and shape type.
+
+    :param str mode: either 'read' or 'write'
+    :param int _type: Esri shape type associated with the file
+    :param typing.Type[shp.ShapefileInterface] kls: the associated class
+    :return: None
+    """
 
     _ModeFactory().register(mode, _type, kls)
+
+
+def construct(mode: str, _type: int) -> typing.Type[shp.ShapefileInterface]:
+    """Retrieve the appropriate ShapefileReader or ShapefileWriter class based on its shape type.
+
+    :param str mode: either 'read' or 'write'
+    :param int _type: the Esri shape type associated with the file
+    :return: the class that is desired to be constructed
+    """
+
+    return _ModeFactory().modes[(mode, _type)]
